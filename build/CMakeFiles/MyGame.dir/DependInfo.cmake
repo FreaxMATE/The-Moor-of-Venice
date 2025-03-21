fdcbd05e74ebd0cb0bf3cf368ead5ca2fa7e6f6d
Konstantin Unruh <freaxmate@protonmail.com>
2025-03-22 00:58:41 +0100

    add brass

---
+++ build/CMakeFiles/MyGame.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kunruh/src/The-Moor-of-Venice/src/AI.cpp" "CMakeFiles/MyGame.dir/src/AI.cpp.o" "gcc" "CMakeFiles/MyGame.dir/src/AI.cpp.o.d"
+  "/home/kunruh/src/The-Moor-of-Venice/src/Board.cpp" "CMakeFiles/MyGame.dir/src/Board.cpp.o" "gcc" "CMakeFiles/MyGame.dir/src/Board.cpp.o.d"
+  "/home/kunruh/src/The-Moor-of-Venice/src/Game.cpp" "CMakeFiles/MyGame.dir/src/Game.cpp.o" "gcc" "CMakeFiles/MyGame.dir/src/Game.cpp.o.d"
+  "/home/kunruh/src/The-Moor-of-Venice/src/ParticleSystem.cpp" "CMakeFiles/MyGame.dir/src/ParticleSystem.cpp.o" "gcc" "CMakeFiles/MyGame.dir/src/ParticleSystem.cpp.o.d"
+  "/home/kunruh/src/The-Moor-of-Venice/src/main.cpp" "CMakeFiles/MyGame.dir/src/main.cpp.o" "gcc" "CMakeFiles/MyGame.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
